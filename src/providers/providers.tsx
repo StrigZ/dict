7c4e871ce6b5e183d934572157757ae35b6e217e
StrigZ <mirgasan200@gmail.com>
2025-02-17 11:44:11 +0300

    Add and config shadcn, tailwind typography, next-themes
    Move components out of app folder

---
+++ src/providers/providers.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ThemeProvider } from "./theme-provider";
+
+type Props = { children: React.ReactNode };
+export default function Providers({ children }: Props) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
