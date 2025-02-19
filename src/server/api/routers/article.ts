8e1c65497378ce2c63658a1d7fe13c85dc4b353f
StrigZ <mirgasan200@gmail.com>
2025-02-19 11:28:33 +0300

    Add articles backend

--- src/server/api/routers/article.ts
+++ src/server/api/routers/article.ts
@@ -12,6 +12,28 @@ export const articleRouter = createTRPCRouter({
       where: { createdById: ctx.session.user.id },
     });
   }),
+  getSingle: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.article.findUnique({
+        where: { id: input.id },
+      });
+    }),
+  getByLetter: protectedProcedure
+    .input(z.object({ startsWith: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.article.findMany({
+        where: { title: { startsWith: input.startsWith, mode: 'insensitive' } },
+      });
+    }),
+  getStartingLetters: protectedProcedure.query(async ({ ctx }) => {
+    return ctx.db.$queryRaw`
+    SELECT DISTINCT UPPER(SUBSTRING(title, 1, 1)) AS letter
+    FROM "public"."Article"
+    ORDER BY letter;
+  ` as unknown as { letter: string }[];
+  }),
+
   create: protectedProcedure
     .input(z.object({ title: z.string().min(1), content: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -21,6 +43,34 @@ export const articleRouter = createTRPCRouter({
           title: input.title,
           content: input.content,
         },
+      });
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        title: z.string().optional(),
+        content: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.article.update({
+        where: { id: input.id },
+        data: {
+          title: input.title,
+          content: input.content,
+        },
+      });
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.article.delete({
+        where: { id: input.id },
       });
     }),
 });
