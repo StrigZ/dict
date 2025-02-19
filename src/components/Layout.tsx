71aaa969c3644d168e4e72bab52b0dd4dbccbe9a
StrigZ <mirgasan200@gmail.com>
2025-02-19 19:01:34 +0300

    Redirect user to login page if not authorized

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,20 @@
+import { redirect } from 'next/navigation';
+
+import { auth } from '~/server/auth';
+
 import Header from './Header';
 import { NewArticleDrawerDialog } from './NewArticleDrawerDialog';
 import Sidebar from './Sidebar';
 import { Toaster } from './ui/toaster';
 
 type Props = { children: React.ReactNode };
-export default function Layout({ children }: Props) {
+export default async function Layout({ children }: Props) {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect('/api/auth/signin');
+  }
+
   return (
     <main className="container relative mx-auto flex h-screen flex-col overflow-hidden">
       <Header />
