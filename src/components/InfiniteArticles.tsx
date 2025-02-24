fb62e7cb82a94cabb890434f9e7d867bb955ccff
StrigZ <mirgasan200@gmail.com>
2025-02-24 18:37:26 +0300

    Use react window for infinite scrolling in sidebar

---
+++ src/components/InfiniteArticles.tsx
@@ -0,0 +1,109 @@
+import Link from 'next/link';
+import { type CSSProperties, useMemo } from 'react';
+import AutoSizer from 'react-virtualized-auto-sizer';
+import { FixedSizeList } from 'react-window';
+import InfiniteLoader from 'react-window-infinite-loader';
+
+import { cn } from '~/lib/utils';
+import { useBreadcrumbsContext } from '~/providers/breadcrumbs-provider';
+import { api } from '~/trpc/react';
+
+import LoadingSpinner from './ui/LoadingSpinner';
+import { buttonVariants } from './ui/button';
+import { useSidebar } from './ui/sidebar';
+
+export default function InfiniteArticles() {
+  const { setOpenMobile } = useSidebar();
+  const { activeArticle, activeLetter } = useBreadcrumbsContext();
+  const {
+    data: articlesQueryData,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = api.article.getInfiniteArticlesByLetter.useInfiniteQuery(
+    { startsWith: activeLetter ?? '', limit: 20 },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
+  const articles = useMemo(() => {
+    return articlesQueryData?.pages.map((page) => page.items).flat() ?? [];
+  }, [articlesQueryData?.pages]);
+
+  const itemCount = hasNextPage ? articles.length + 1 : articles.length;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const loadMoreItems = isFetchingNextPage ? () => {} : () => fetchNextPage();
+
+  const isItemLoaded = (index: number) =>
+    !hasNextPage || index < articles.length;
+
+  const Article = ({
+    index,
+    style,
+  }: {
+    index: number;
+    style: CSSProperties;
+  }) => {
+    const article = articles[index];
+
+    let content;
+    if (!isItemLoaded(index)) {
+      content = (
+        <div className="flex items-center justify-center" style={style}>
+          <LoadingSpinner />
+        </div>
+      );
+    } else {
+      content = (
+        <Link
+          key={article?.id}
+          href={'/articles/' + article?.id}
+          style={style}
+          className={cn(
+            buttonVariants({
+              variant: article?.id === activeArticle?.id ? 'default' : 'ghost',
+              className:
+                'w-full justify-start truncate rounded-none sm:justify-center sm:py-6',
+            }),
+          )}
+          onClick={() => setOpenMobile(false)}
+        >
+          {article?.title.split(' ')[0]}
+        </Link>
+      );
+    }
+
+    return content;
+  };
+
+  return (
+    <div className="flex-1">
+      <AutoSizer>
+        {({ height, width }) => {
+          return (
+            <InfiniteLoader
+              isItemLoaded={isItemLoaded}
+              itemCount={itemCount}
+              loadMoreItems={loadMoreItems}
+            >
+              {({ onItemsRendered, ref }) => (
+                <FixedSizeList
+                  itemCount={itemCount}
+                  onItemsRendered={onItemsRendered}
+                  ref={ref}
+                  itemSize={48}
+                  height={height}
+                  width={width}
+                  className="scrollbar-thumb-rounded-full w-full border-x border-l scrollbar-thin scrollbar-track-transparent sm:border-x sm:pb-12"
+                >
+                  {Article}
+                </FixedSizeList>
+              )}
+            </InfiniteLoader>
+          );
+        }}
+      </AutoSizer>
+    </div>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,63 +1,18 @@
 'use client';
 
-import Link from 'next/link';
-import { useEffect, useMemo, useRef } from 'react';
-
 import { cn } from '~/lib/utils';
 import { useBreadcrumbsContext } from '~/providers/breadcrumbs-provider';
 import { api } from '~/trpc/react';
 
+import InfiniteArticles from './InfiniteArticles';
 import SidebarSkeleton from './skeletons/SidebarSkeleton';
-import LoadingSpinner from './ui/LoadingSpinner';
-import { Button, buttonVariants } from './ui/button';
+import { Button } from './ui/button';
 import { ScrollArea } from './ui/scroll-area';
-import { useSidebar } from './ui/sidebar';
 
 export default function Sidebar({ className }: { className?: string }) {
-  const { activeArticle, activeLetter, selectActiveLetter } =
-    useBreadcrumbsContext();
-  const { setOpenMobile } = useSidebar();
+  const { activeLetter, selectActiveLetter } = useBreadcrumbsContext();
   const { data: startingLetters, isLoading: isLettersQueryLoading } =
     api.article.getStartingLetters.useQuery();
-
-  const {
-    data: articlesQueryData,
-    isLoading: isArticleQueryLoading,
-    isFetchingNextPage,
-    fetchNextPage,
-  } = api.article.getInfiniteArticlesByLetter.useInfiniteQuery(
-    { startsWith: activeLetter ?? '', limit: 25 },
-    {
-      getNextPageParam: (lastPage) => lastPage.nextCursor,
-    },
-  );
-
-  const listRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!listRef.current) {
-      return;
-    }
-    const element = listRef.current;
-
-    const handleDropdownScroll = () => {
-      const { scrollHeight, scrollTop, clientHeight } = element;
-
-      if (scrollHeight - scrollTop === clientHeight) {
-        void fetchNextPage();
-      }
-    };
-
-    element.addEventListener('scroll', handleDropdownScroll);
-
-    return () => {
-      element.removeEventListener('scroll', handleDropdownScroll);
-    };
-  }, [listRef, fetchNextPage]);
-
-  const articles = useMemo(() => {
-    return articlesQueryData?.pages.map((page) => page.items).flat();
-  }, [articlesQueryData?.pages]);
 
   return isLettersQueryLoading ? (
     <SidebarSkeleton />
@@ -74,7 +29,6 @@ export default function Sidebar({ className }: { className?: string }) {
                   'bg-primary text-primary-foreground': letter === activeLetter,
                 })}
                 onClick={() => selectActiveLetter(letter)}
-                disabled={isArticleQueryLoading}
               >
                 {letter}
               </Button>
@@ -83,41 +37,7 @@ export default function Sidebar({ className }: { className?: string }) {
         </ul>
       </ScrollArea>
 
-      <ScrollArea
-        className="relative h-full flex-1 border-l sm:border-x [&>div>div]:!block"
-        viewportRef={listRef}
-      >
-        <ul className="h-full sm:pb-12">
-          {isArticleQueryLoading && (
-            <div className="absolute inset-0 flex items-center justify-center">
-              <LoadingSpinner />
-            </div>
-          )}
-          {articles?.map((article) => (
-            <li key={article.id}>
-              <Link
-                href={'/articles/' + article.id}
-                className={cn(
-                  buttonVariants({
-                    variant:
-                      article.id === activeArticle?.id ? 'default' : 'ghost',
-                    className:
-                      'w-full justify-start truncate rounded-none sm:justify-center sm:py-6',
-                  }),
-                )}
-                onClick={() => setOpenMobile(false)}
-              >
-                {article.title.split(' ')[0]}
-              </Link>
-            </li>
-          ))}
-          {isFetchingNextPage && (
-            <div className="flex w-full items-center justify-center p-2">
-              <LoadingSpinner />
-            </div>
-          )}
-        </ul>
-      </ScrollArea>
+      <InfiniteArticles />
     </aside>
   );
 }
