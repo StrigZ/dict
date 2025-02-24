b5d88d8e8203cc0bb40a7410777eef3e1c771a7f
StrigZ <mirgasan200@gmail.com>
2025-02-24 15:58:14 +0300

    Increase drawer icon size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 export default async function Header() {
   return (
     <header className="container mx-auto flex items-center justify-center gap-4 p-4">
-      <SidebarTrigger className="sm:hidden" />
+      <SidebarTrigger className="h-10 w-10 sm:hidden" />
       <ThemeButton className="hidden sm:flex" />
       <SearchBar />
       <LoginButton className="hidden sm:flex" />
